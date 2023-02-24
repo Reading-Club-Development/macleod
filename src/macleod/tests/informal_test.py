a1bb7bcbd73f4a71015f973445eac4d064760a87
Gunnar Eastman <gunnar.eastman@gmail.com>
2023-02-24 17:21:45 -0500

    trying to mess with testing a little bit

---
+++ src/macleod/tests/informal_test.py
@@ -0,0 +1,3 @@
+import Macleod.scripts.parser as parser
+
+parser.parse_clif("rcc_basic_old.clif")
--- src/macleod/tests/test_Parser.py
+++ src/macleod/tests/test_Parser.py
@@ -5,12 +5,14 @@ from Macleod.logical.axiom import Axiom
 from Macleod.logical.quantifier import (Universal, Existential, Quantifier)
 import Macleod.Ontology as Ontology
 
+import Macleod.scripts.parser as Parser
+
 class OnologyTest(unittest.TestCase):
     """
     Test functionality of Ontology class
     """
 
-    def test_owl_subclass(self):
+    def test_parse(self):
         a = Predicate('A', ['x'])
         b = Predicate('B', ['x'])
         c = Predicate('C', ['x'])
@@ -19,7 +21,8 @@ class OnologyTest(unittest.TestCase):
 
         onto = Ontology("Derp")
         onto.axioms.append(subclass_relation)
-        print(onto.to_owl(0))
+
+        Parser.parse_clif("rcc_basic.clif")
 
 if __name__ == '__main__':
     unittest.main()
